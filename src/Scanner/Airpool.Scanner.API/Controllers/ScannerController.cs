4a24f372304a0c326c7d7a3e450d4d86336c260f
igorcaptain <gmalashevich09@gmail.com>
2021-04-10 22:20:22 +0300

    Add project files.

---
+++ src/Scanner/Airpool.Scanner.API/Controllers/ScannerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airpool.Scanner.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScannerController : ControllerBase
+    {
+
+    }
+}
